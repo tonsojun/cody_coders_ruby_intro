26173a188e84683c18cc46c36b26507a4bc4e4bc
tonsojun <josephjun@gmail.com>
2021-02-22 22:04:18 -0800

    Completed intro ex 4 and 5 without tests.  Will complete test.

---
+++ five.rb
@@ -0,0 +1,62 @@
+names = ["mark", "aaron", "steven", "mary", "hermitcrab"]
+
+def print_names_ending_with_n(names)
+  #iterate over names array
+  names.select do |name|
+    #check to see if the last letter of the string is "n"
+    if(name[name.length - 1] == "n")
+      #print name that has last letter "n"
+      print name
+    end
+  end
+end
+
+puts print_names_ending_with_n(names)
+
+# This is a classic problem:
+
+=begin
+Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” 
+instead of the number and for the multiples of five print “Buzz”.
+For numbers which are multiples of both three and five print “FizzBuzz”
+=end
+
+def fizzbuzz()
+  #create a loop from 1 to 100
+  for i in 1..100
+    #for multiples of 3 print Fizz
+    if(i % 3 == 0)
+      print "Fizz"
+      #for multiples of 5 print Buzz
+    elsif(i % 3 == 0)
+      print "Buzz"
+      #for multiples of both 3 and 5 print FizzBuzz
+    else(i % 3 == 0 && i % 5 == 0)
+      print "FizzBuzz"
+    end
+  end
+end
+
+puts fizzbuzz()
+
+
+# write a method that takes a string as a parameter. return the string in reverse using a for loop (not ruby's .reverse method). 
+def reverseString(string)
+  #create an empty storage string to build
+  reversed_string = ""
+  #iterate over the string
+  i = 0
+  while i < string.length
+  #place characters in front of the current character
+    reversed_string = string[i] + reversed_string
+    i += 1
+  end
+  #return built string
+  print reversed_string
+end
+
+nameString = 'Joe'
+puts reverseString(nameString)
+
+#write assertion test
+
--- four.rb
+++ four.rb
@@ -1,13 +1,47 @@
+
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
+# Write a method, which given two arrays, one of student names, one of student languages, returns a hash like this:
+
+# your_hash = {
+#   "mark" => "ruby",
+#   "aaron" => 'javascript'
+# }
+
+# If the two arrays are not the same length, return false. Write a test using assert_equal!!!
+# write a test for happy path - input is normal and returns the expected return value
+# and for sad path - input has mismatched arrays. Google happy path sad path testing
+
 def student_languages(arr1, arr2)
-  if(arr1.length == arr2.length)
+  if(arr1.length != arr2.length)
+    return false
+  else
     combined = Hash[arr1.zip(arr2)]
     puts combined
-  else
-    return false
   end  
 end
 
-students = ["Joe", "Kalei", "Tristan"]
-language = ["Ruby", "Javascript", "Python"]
+students = ["Joe", "Kalei", "Tristan", "Vernon", "Melissa"]
+language = ["Ruby", "Javascript", "Python", "Ruby", "C++"]
 
 student_languages(students, language)
+
+# Write a method which takes a hash in the format as this:
+
+# your_hash = {
+#   "mark" => "ruby",
+#   "aaron" => 'javascript,
+#   "geo" => "ruby"
+# }
+
+# Then, print the names of all of the students who have the language ruby. Make sure you have like 5 examples in the array. no test here
+
+def ruby_devs(dev_hash)
+  puts dev_hash.select {|student, language| language == "Ruby"}
+end
+
+ruby_devs({"Joe"=>"Ruby", "Kalei"=>"Javascript", "Tristan"=>"Python", "Vernon"=>"Ruby", "Melissa"=>"C++"})
+
+
+assert_equal(arr1.length == arr2.length, student_languages(students, language), "arr1 and arr2 are the same length")
