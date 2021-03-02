e0aa6972b184ddc776a55daf464093f30a0dfc3d
tonsojun <josephjun@gmail.com>
2021-03-02 12:17:12 -0800

    POST request for assignment 7

--- seven.rb
+++ seven.rb
@@ -4,12 +4,9 @@ response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackov
 
 options = {
   body: {
-    pear: { # your resource
-      foo: '123', # your columns/data
-      bar: 'second',
-      baz: 'last thing'
-    }
+    my_name: "Mark",
+    my_hobby: "Soccer"
   }
 }
 
-puts HTTParty.get('https://webhook.site/e321c91d-5ea8-43f8-a786-a5b4a7e26052', options)
+puts HTTParty.post('https://webhook.site/e321c91d-5ea8-43f8-a786-a5b4a7e26052', options)
