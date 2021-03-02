0ecb4da46e843e35fd94daee4dfb2f29bbe4e18a
tonsojun <josephjun@gmail.com>
2021-03-02 12:29:24 -0800

    Final POST request assign 7

--- seven.rb
+++ seven.rb
@@ -1,10 +1,14 @@
 require 'httparty'
+require 'active_support/all'
 
 response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
 
 options = {
   body: {
-    current_time: Time.now
+    current_time: Time.now,
+    five_days_ago: 5.days.ago,
+    one_year_ago: 1.year.ago,
+    two_years_from_now: 2.years.from_now
   }
 }
 
